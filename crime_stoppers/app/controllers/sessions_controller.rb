cf3a1c31987da5d8042f06e9ea6e2e5c034da820
Khalid Alsuwaiyel <alsuwaiyelk@gmail.com>
2014-09-09 03:22:56 -0400

    Finish user sign up, sign in and sign out with session control

---
+++ crime_stoppers/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      @current_user = user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Signed in!"
+    else
+      flash[:error] = "Invalid email or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Signed out!"
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- crime_stoppers/app/models/user.rb
+++ crime_stoppers/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
    has_secure_password 
 
+   validates_uniqueness_of :email
+   validates_presence_of :email
+   validates_presence_of :password, :on => :create
+   
    def self.find_or_create_from_email(email)
      password = SecureRandom.base64
      User.where(email: email.downcase).first_or_create!(password: password, password_confirmation: password)
