5881a7f80798ba6a680a41e488d677c5961ebf9d
Khalid Alsuwaiyel <alsuwaiyelk@gmail.com>
2014-10-29 06:49:58 -0400

    Import data from open-data-cincy

--- crime_stoppers/app/controllers/crime_records_controller.rb
+++ crime_stoppers/app/controllers/crime_records_controller.rb
@@ -10,7 +10,13 @@ class CrimeRecordsController < ApplicationController
   end
   
   def import
-    CrimeRecord.import(params[:file])
+    path = '/home/ec2-user/csc440/csc440/crime_stoppers/public/cpd.zip'
+    File.write(path, open( 'http://www.opendatacincy.org/static/cincinnati/cpd.zip' ).read, {mode: 'wb'})
+    `sudo unzip -o /home/ec2-user/csc440/csc440/crime_stoppers/public/cpd.zip -d /home/ec2-user/`
+    Dir.glob('/home/ec2-user/cpd/*.csv').each do |file|
+        debugger
+      CrimeRecord.import(file)
+    end
     redirect_to root_url, notice: "Records imported."
   end
 
--- crime_stoppers/app/models/crime_record.rb
+++ crime_stoppers/app/models/crime_record.rb
@@ -1,16 +1,18 @@
 require 'csv'
+require 'open-uri'
+
 class CrimeRecord < ActiveRecord::Base
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-	crime_record = CrimeRecord.new
+    CSV.foreach(file, headers: true) do |row|
+        crime_record = CrimeRecord.new
         data = row.to_s.split(',')
         location = Location.find_or_create_from_address(data[2])
         crime_type = CrimeType.find_or_create_from_name(data[3])
-	crime_record.date = data[0]
-	crime_record.event_id = data[1]
-	crime_record.crime_type_id = crime_type.id
-	crime_record.location_id = location.id
-	crime_record.save
+        crime_record.date = data[0]
+        crime_record.event_id = data[1]
+        crime_record.crime_type_id = crime_type.id
+        crime_record.location_id = location.id
+        crime_record.save
     end
   end
 end
