c848c5ac82c2e2a5a3551f31e6ef5dd48dcb375d
Khalid Alsuwaiyel <alsuwaiyelk@gmail.com>
2014-10-28 21:51:27 -0400

    Fix rake bug

--- crime_stoppers/config/application.rb
+++ crime_stoppers/config/application.rb
@@ -4,6 +4,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
