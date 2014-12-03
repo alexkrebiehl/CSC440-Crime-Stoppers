527d9410c2470c69a64325d810a3d9824d0a6bf9
Khalid Alsuwaiyel <alsuwaiyelk@gmail.com>
2014-12-03 00:56:50 +0000

    Store email and password as an environment constants

--- crime_stoppers/config/environment.rb
+++ crime_stoppers/config/environment.rb
@@ -7,7 +7,7 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'crime2stoppers@gmail.com',
-  password:             'crime2stoppers1',
+  user_name:            ENV["EMAIL"],
+  password:             ENV["PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
